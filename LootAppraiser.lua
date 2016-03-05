352accce8fe69c0f1bd2e654623c314aef6067e7
Juijang <sven@rottenbiller.de>
2016-03-05 19:06:09 +0100

    fix
    print_r via D

--- LootAppraiser.lua
+++ LootAppraiser.lua
@@ -1593,34 +1593,34 @@ function LA:print_r ( t )
     local print_r_cache={}
     local function sub_print_r(t,indent)
         if (print_r_cache[tostring(t)]) then
-            print(indent.."*"..tostring(t))
+            LA:D(indent.."*"..tostring(t))
         else
             print_r_cache[tostring(t)]=true
             if (type(t)=="table") then
                 for pos,val in pairs(t) do
                     if (type(val)=="table") then
-                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        LA:D(indent.."["..pos.."] => "..tostring(t).." {")
                         sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                        LA:D(indent..string.rep(" ",string.len(pos)+6).."}")
                     elseif (type(val)=="string") then
-                        print(indent.."["..pos..'] => "'..val..'"')
+                        LA:D(indent.."["..pos..'] => "'..val..'"')
                     else
-                        print(indent.."["..pos.."] => "..tostring(val))
+                        LA:D(indent.."["..pos.."] => "..tostring(val))
                     end
                 end
             else
-                print(indent..tostring(t))
+                LA:D(indent..tostring(t))
             end
         end
     end
     if (type(t)=="table") then
-        print(tostring(t).." {")
+        LA:D(tostring(t).." {")
         sub_print_r(t,"  ")
-        print("}")
+        LA:D("}")
     else
         sub_print_r(t,"  ")
     end
-    print()
+    LA:D()
 end
 
 --[[-------------------------------------------------------------------------------------
